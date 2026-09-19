be7705777e058c5c997e1f771094940244792342
3ColumnsC <3columnscreations@protonmail.com>
2026-09-19 09:48:30 +0200

    Clean

--- common/src/main/java/com/threecolumnsstudio/nomobsthankyou/config/ConfigOverrides.java
+++
@@ -1,9 +0,0 @@
-package com.threecolumnsstudio.nomobsthankyou.config;
-
-import java.util.Collections;
-import java.util.List;
-
-public class ConfigOverrides {
-    public List<String> remove = Collections.emptyList();
-    public List<String> keep = Collections.emptyList();
-}
--- common/src/main/java/com/threecolumnsstudio/nomobsthankyou/config/ConfigPresets.java
+++
@@ -1,8 +0,0 @@
-package com.threecolumnsstudio.nomobsthankyou.config;
-
-public class ConfigPresets {
-    public Boolean disableAllMobs = null;
-    public Boolean onlyVillagers = null;
-    public Boolean disableVillagers = null;
-    public Boolean disableBosses = null;
-}
